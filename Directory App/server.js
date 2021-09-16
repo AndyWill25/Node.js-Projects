cb157bed486ec48e108ce35d918b7fa9a651d633
AndyWill25 <andymac2523@gmail.com>
2021-09-16 12:06:32 -0500

    Added modules, connected to Mongo

--- Directory App/server.js
+++ Directory App/server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 
@@ -5,8 +9,19 @@ app.use(express.static('Public'))
 
 const PORT = process.env.PORT || 3000
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+
+
 app.get('/', (req, res)=>{
-    res.send('I am ROOT')
+    res.render('index.ejs')
 })
 
 app.listen(PORT, () => {
